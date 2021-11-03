d0f29f38994a550c283db58a4cba9e04f597772f
AJotaErre <junioralu2000@gmail.com>
2021-11-03 17:29:22 -0300

    Update leitortabela.py
    Adicionados linhas de códigos para manusear as bases de dados do excel.
    
    OBS: estou tentando organizar as datas pra ficar mais fácil de visualizar nas tabelas.

--- leitortabela.py
+++ leitortabela.py
@@ -6,7 +6,15 @@ import numpy as np
 #Sheet_name serve pra selecionar a aba da planilha
 #usecols serve pra selecionar as colunas (começa do 0)
 # Não esquecer de printar antes pra ver se ta dando certo!
-tabela = pd.read_excel('testepib.xls', sheet_name='CEI', usecols=[0,1])
-print(tabela)
 
-"tabela.to_excel('resultadopib.xls')"
+df = pd.read_excel('testepib.xls', sheet_name='CEI', usecols=[0,1], header=[2])
+df2 = pd.read_excel('testeipca.xlsx', usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12], header=[11])
+df.columns = ['Período', 'Valor']
+dfanos = df.loc[{4,9,14,19,24,29,34,39,44,49,54,59,64,69,74,79,84,89,94,99,104}]
+#dfanos.sort_index(ascending=True) -- Tentando organizar as index
+#df.to_excel('resultadopib.xlsx') -- Converter pra tabela em xlsx
+#print(dfanos)
+
+df = df.drop([4,9,14,19,24,29,34,39,44,49,54,59,64,69,74,79,84,89,94,99,104])
+#print(df) -- print do data frame
+#df2.to_excel('resultadoipca.xlsx') -- Converter IPCA pra tabela em xlsx
