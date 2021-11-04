30cc1b12d2ae93ecd3e7a0ea43969b7be1392043
Gabe <gabriel_msnid@hotmail.com>
2021-11-04 18:45:32 -0300

    muita coisa
    junção dos arquivos, vou deixar pra cuspir o ultimo arquivo depois
    
    comecei a extração do zip mas decidi deixar pra amanha
    
    ta tudo funcionando so falta deixar bonitinho e emperiquitar o codigo
    
    tem que excluir o leitor de tabela.py

--- leitortabela.py
+++ leitortabela.py
@@ -6,7 +6,18 @@ import numpy as np
 #Sheet_name serve pra selecionar a aba da planilha
 #usecols serve pra selecionar as colunas (começa do 0)
 # Não esquecer de printar antes pra ver se ta dando certo!
-tabela = pd.read_excel('testepib.xls', sheet_name='CEI', usecols=[0,1])
+tabela = pd.read_excel('C:/Users/RR854YA/Downloads/testepib.xls', sheet_name='CEI', usecols=[0,1])
+
+
+tabela.columns = ['Período', 'Valor em Trilhão de R$']
+tabela = tabela.drop([0, 1,109])
+tabela['Valor em Trilhão de R$'] = tabela['Valor em Trilhão de R$'].astype(np.int64)
+
+
+
+
+tabela['Valor em Trilhão de R$'] = (tabela['Valor em Trilhão de R$']/1000000).apply(lambda x: '{:,.2f}'.format(x))
+#tabela
 print(tabela)
 
-"tabela.to_excel('resultadopib.xls')"
+#tabela.to_excel('resultadopib2.xls')
